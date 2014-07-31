760d0411cad9ececf8b8e003923e19a9dcecc5bc
Patrick Narkinsky <jnarki001c@hqsml-144079.cable.comcast.com>
2014-07-31 12:21:13 -0400

    Added .profile

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 for i in .*
 do 
-    if [ "$i" != '.hg' -a "$i" != '..' -a "$i" != '.' ]; then
+    if [ "$i" != '.git' -a "$i" != '.gitignore' -a "$i" != '..' -a "$i" != '.' ]; then
 	echo Installing $i
 	ln -s `pwd`/$i $HOME/$i
     fi
